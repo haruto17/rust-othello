66c5c6ee762cec8e7edcd099d2957c854674d3f4
haruto17 <s1902272@sendai-nct.jp>
2023-10-17 10:40:49 +0900

    盤のサイズを定数化

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ use proconio::input;
 // o == white
 // x == black
 
-fn init_board(board: &mut [[char; 8]; 8]) {
+const BOARD_HEIGHT: usize = 8;
+const BOARD_WIDTH: usize = 8;
+
+fn init_board(board: &mut [[char; BOARD_WIDTH]; BOARD_HEIGHT]) {
     board[3][3] = 'o';
     board[3][4] = 'x';
     board[4][3] = 'x';
     board[4][4] = 'o';
 }
 
-fn draw_board(board: &[[char; 8]; 8]) {
+fn draw_board(board: &[[char; BOARD_WIDTH]; BOARD_HEIGHT]) {
     for i in 0..8 {
         for j in 0..8 {
             print!("{}", board[i][j]);
@@ -18,7 +21,7 @@ fn draw_board(board: &[[char; 8]; 8]) {
     }
 }
 
-fn put_stone(board: &mut [[char; 8]; 8], player: usize, x: usize, y: usize) {
+fn put_stone(board: &mut [[char; BOARD_WIDTH]; BOARD_HEIGHT], player: usize, x: usize, y: usize) {
     if player == 1 {
         board[x][y] = 'x';
     } else {
@@ -33,7 +36,7 @@ fn check_can_put(x: usize, y: usize) -> Result<(), ()> {
 fn main() {
     let mut turn = 1;
 
-    let mut board: [[char; 8]; 8] = [['-'; 8]; 8];
+    let mut board: [[char; BOARD_WIDTH]; BOARD_HEIGHT] = [['-'; BOARD_WIDTH]; BOARD_HEIGHT];
     init_board(&mut board);
 
     loop {
