ff2f0efa13070ef7e5158c95e53c7fa2c4cb7eb2
haruto17 <s1902272@sendai-nct.jp>
2023-10-18 11:19:27 +0900

    チェック用関数の処理

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,160 @@ fn put_stone(board: &mut [[char; BOARD_WIDTH]; BOARD_HEIGHT], player: usize, x: 
     }
 }
 
-fn check_can_put(x: usize, y: usize) -> Result<(), ()> {
-    Ok(())
+fn check_can_put(
+    board: &[[char; BOARD_WIDTH]; BOARD_HEIGHT],
+    player: char,
+    x: &usize,
+    y: &usize,
+) -> Result<(), ()> {
+    // N
+    let mut check_n: bool = false;
+    let n_x = *x;
+    let mut n_y = *y;
+    while board[n_x][n_y] != '#' {
+        if board[n_x][n_y] == player {
+            if check_n {
+                return Ok(());
+            }
+        }
+
+        if board[n_x][n_y] != player {
+            check_n = true;
+        }
+
+        n_y -= 1;
+    }
+
+    // NE
+    let mut check_ne: bool = false;
+    let mut ne_x = *x;
+    let mut ne_y = *y;
+    while board[ne_x][ne_y] != '#' {
+        if board[ne_x][ne_y] == player {
+            if check_ne {
+                return Ok(());
+            }
+        }
+
+        if board[ne_x][ne_y] != player {
+            check_ne = true;
+        }
+
+        ne_x += 1;
+        ne_y -= 1;
+    }
+
+    // E
+    let mut check_e: bool = false;
+    let mut e_x = *x;
+    let e_y = *y;
+    while board[e_x][e_y] != '#' {
+        if board[e_x][e_y] == player {
+            if check_e {
+                return Ok(());
+            }
+        }
+
+        if board[e_x][e_y] != player {
+            check_e = true;
+        }
+
+        e_x += 1;
+    }
+
+    // SE
+    let mut check_se: bool = false;
+    let mut se_x = *x;
+    let mut se_y = *y;
+    while board[se_x][se_y] != '#' {
+        if board[se_x][se_y] == player {
+            if check_se {
+                return Ok(());
+            }
+        }
+
+        if board[se_x][se_y] != player {
+            check_se = true;
+        }
+
+        se_x += 1;
+        se_y += 1;
+    }
+
+    // S
+    let mut check_s: bool = false;
+    let s_x = *x;
+    let mut s_y = *y;
+    while board[s_x][s_y] != '#' {
+        if board[s_x][s_y] == player {
+            if check_s {
+                return Ok(());
+            }
+        }
+
+        if board[s_x][s_y] != player {
+            check_s = true;
+        }
+
+        s_y += 1;
+    }
+
+    // SW
+    let mut check_sw: bool = false;
+    let mut sw_x = *x;
+    let mut sw_y = *y;
+    while board[sw_x][sw_y] != '#' {
+        if board[sw_x][sw_y] == player {
+            if check_sw {
+                return Ok(());
+            }
+        }
+
+        if board[sw_x][sw_y] != player {
+            check_sw = true;
+        }
+
+        sw_x -= 1;
+        sw_y += 1;
+    }
+
+    // W
+    let mut check_w: bool = false;
+    let mut w_x = *x;
+    let w_y = *y;
+    while board[w_x][w_y] != '#' {
+        if board[w_x][w_y] == player {
+            if check_w {
+                return Ok(());
+            }
+        }
+
+        if board[w_x][w_y] != player {
+            check_w = true;
+        }
+
+        w_x -= 1;
+    }
+
+    // NW
+    let mut check_nw: bool = false;
+    let mut nw_x = *x;
+    let mut nw_y = *y;
+    while board[nw_x][nw_y] != '#' {
+        if board[nw_x][nw_y] == player {
+            if check_nw {
+                return Ok(());
+            }
+        }
+
+        if board[nw_x][nw_y] != player {
+            check_nw = true;
+        }
+
+        nw_x -= 1;
+        nw_y -= 1;
+    }
+    Err(())
 }
 
 fn main() {
@@ -56,7 +208,7 @@ fn main() {
                 x: usize,
                 y: usize,
             }
-            match check_can_put(x, y) {
+            match check_can_put(&board, BLACK, &x, &y) {
                 Ok(()) => {
                     put_stone(&mut board, 1, x, y);
                     turn += 1;
@@ -70,7 +222,7 @@ fn main() {
                 x: usize,
                 y: usize,
             }
-            match check_can_put(x, y) {
+            match check_can_put(&board, WHITE, &x, &y) {
                 Ok(()) => {
                     put_stone(&mut board, 2, x, y);
                     turn += 1;
